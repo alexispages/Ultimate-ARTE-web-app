4cd4ce4ad38642cabb928fd5409a6649ebec1a2f
Nilemme57 <anatolewilke@gmail.com>
2022-12-14 14:57:59 +0100

    Ajout de flask, et request sur l'API arte

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+import requests
+name="main"
+app = Flask(name)
+
+@app.route('/arte')
+def get_arte_data():
+    #url = 'https://api.arte.tv/api/opa/v3/works?fields=id,title,shortDescription,previewPictures'
+    url = 'https://api.arte.tv/api/player/v2/playlist/fr/LIVE?'
+    response = requests.get(url)
+    return jsonify(response.json())
+
+if name == 'main':
+    app.run()
