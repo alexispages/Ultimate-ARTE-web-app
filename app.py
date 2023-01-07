1b20dea139a7e6db6caea6bf0daebe0a0bf6ba4d
Nilemme57 <anatolewilke@gmail.com>
2023-01-07 22:13:20 +0100

    resolution des pb que j'ai eu avec un rebase

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, redirect, request
+#
 import lesfonctions
+import re
+
+
 name="main"
 app = Flask(name)
+partage_data=""
+
+@app.route('/arte', methods=['GET', 'POST'])
+def index():
+    try:
+        if request.method == 'POST':
+            requestresult=str(request.form)
+            if re.search("jour", requestresult)!= None:
+                return redirect('/arte/programme_du_jour')
+            elif re.search("categories", requestresult)!= None:
+                return redirect('/arte/categories')
+            else:
+                return render_template('index.html')
+        return render_template('index.html')
+    
+    except Exception as error:
+        print(error)
+
+
 
 @app.route('/arte/programme_du_jour')
 def get_arte_programme_du_jour():
-
     try:
         url = 'https://api.arte.tv/api/player/v2/playlist/fr/LIVE?'
         response=lesfonctions.collect(url)
@@ -16,6 +38,14 @@ def get_arte_programme_du_jour():
     except Exception as error:
         print(error)
 
+
+@app.route('/arte/categories')
+def get_arte_categories():
+    try:
+        return "OK"
+
+    except Exception as error:
+        print(error)
 
 if name == 'main':
     app.run()
