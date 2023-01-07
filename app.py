fa8607f2efc5d6bf25d9966e50898e93854b67ab
Nilemme57 <anatolewilke@gmail.com>
2023-01-07 21:42:59 +0100

    Revert "ajout de la page index"
    This reverts commit 2dcc2d85d2d92648aa1f8b2d979e25e9195b88bb.

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, redirect, request
-import lesfonctions, re
+from flask import Flask, render_template
+import lesfonctions
 name="main"
 app = Flask(name)
 
-@app.route('/arte')
-def get_arte_data():
-    #url = 'https://api.arte.tv/api/opa/v3/works?fields=id,title,shortDescription,previewPictures'
-    url = 'https://api.arte.tv/api/player/v2/playlist/fr/LIVE?'
-    response = requests.get(url)
-    return jsonify(response.json())
+@app.route('/arte/programme_du_jour')
+def get_arte_programme_du_jour():
+
+    try:
+        url = 'https://api.arte.tv/api/player/v2/playlist/fr/LIVE?'
+        response=lesfonctions.collect(url)
+        parse_result=lesfonctions.parse_programme_du_jour(response)
+        table=lesfonctions.generate_table(parse_result)
+        return render_template("programmedujour.html", table=table)
+
+    except Exception as error:
+        print(error)
+
 
 if name == 'main':
     app.run()
