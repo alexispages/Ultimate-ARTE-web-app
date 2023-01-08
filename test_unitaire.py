23abaca4ed91edcaa9ed6a804ef1e81ca6c0044a
Pl@g3 <apages@tutamail.com>
2023-01-08 14:33:09 +0100

    add unitary tests for web pages

--- test_unitaire.py
+++ test_unitaire.py
@@ -7,13 +7,19 @@ class AppTestCase(unittest.TestCase):
         self.app = app.test_client()
         app.testing = True
 
-    def test_home_status_code(self):
+    def test_status_code_index(self):
         result = self.app.get('http://127.0.0.1:5000/arte')
         self.assertEqual(result.status_code, 200)
 
+    def test_status_code_programmedujour(self):
+        result = self.app.get('http://127.0.0.1:5000/arte/programme_du_jour')
+        self.assertEqual(result.status_code, 200)
+
+    def test_status_code_categories(self):
+        result = self.app.get('http://127.0.0.1:5000/arte/categories')
+        self.assertEqual(result.status_code, 200)
 
     def test_table_exists(self):
-
         response = self.app.get('http://127.0.0.1:5000/arte/programme_du_jour')
         soup = BeautifulSoup(response.data, 'html.parser')
         table = soup.find('table')
